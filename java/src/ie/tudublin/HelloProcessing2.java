f0ab55a81d238bbbb51b5368233abe89332e02f8
Maurice Reid <maurice.reid@storebest.ie>
2021-02-01 15:46:01 +0000

    my processes

--- java/src/ie/tudublin/HelloProcessing2.java
+++ java/src/ie/tudublin/HelloProcessing2.java
@@ -16,8 +16,20 @@ public class HelloProcessing2 extends PApplet
 	
 	public void draw()
 	{	
-		background(0);
-		stroke(255);
-		line(10, 10, 100, 100);
+		background(255, 0, 0);
+		fill(255, 255, 0);
+		ellipse(250, 300, 400, 400);
+		
+		fill(0, 255, 255);
+		triangle(250, 50, 450, 450, 50, 450);
+		
+		fill(255,200,200);
+		ellipse(250, 250, 180, 80);
+		
+	
+		fill(0,0,0);
+		ellipse(250,250,70,70);
+		
+
 	}
 }
