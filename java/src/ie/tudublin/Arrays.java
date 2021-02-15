ec26e0ed1988ee3e8dc85114db309f8dac4375fd
Maurice Reid <maurice.reid@storebest.ie>
2021-02-15 16:54:11 +0000

    lecture 4, week 4

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -4,11 +4,37 @@ import processing.core.PApplet;
 
 public class Arrays extends PApplet {
 
+    public float map1 (float from, float start1, float stop1, float start2, float stop2) 
+    {
+        float range1 = stop1 - start1;
+        float range2 = stop2 - start2;
+        float howFar = from - start1;
+
+        return start2 + (howFar / range1) * range2;
+        
+    }
     public void settings() {
-        size(500, 500);                
+        size(500, 500);  
+        
+        float f = map1(2, 0, 10, 0, width);
+        println(f);
+
+        f = map1(9, 0, 1, 0, 10);
+        println(f);
+
+        f = map1(250, 200, 300, 400, 500);
+        println(f);
+
+        f = map1(5, 0, 10, 1000, 2000);
+        println(f);
+
     }
 
     int mode = 0;
+
+    float[] rainfall = {45, 37, 55, 27, 38, 50, 79, 48, 104, 31, 100, 58};
+    String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"};
+    float[] arr = new float[100]; //100 flaot array
 
     public void keyPressed() {
         // the value of mode will be the number of the 
@@ -19,6 +45,39 @@ public class Arrays extends PApplet {
 
     public void setup() {
         colorMode(HSB);
+
+        for (int i = 0 ; i < rainfall.length ; i++)
+        {
+            println(months[i] + "\t" + rainfall[i]);
+        }
+ 
+
+        int minIndex = 0;
+        int maxIndex = 0;
+        float sum = 0;
+
+        for (int i = 0 ; i < rainfall.length ; i ++)
+        {
+            if(rainfall[i] < rainfall[minIndex])
+            {
+                minIndex = i;
+            }
+            
+            if(rainfall[i] > rainfall[maxIndex])
+            {
+                maxIndex = i;
+            }
+            sum += rainfall[i];
+        }
+        float average = sum / (float) rainfall.length;
+        println("Least rain fall was in " + months[minIndex] + " with " + rainfall[minIndex]);
+        println("Most rain fall was in " + months[maxIndex] + " with " + rainfall[maxIndex]);
+        println("Average rainfall average was " + average);
+        //Enhanced for loop
+        /*for (float f:rainfall)
+        {
+            println(f);
+        }*/
     }
 
     float offset = 0;
--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -27,6 +27,7 @@ public class Loops extends PApplet {
     }
 
     float offset = 0;
+    
 
     public void draw() {
         background(0);
@@ -34,21 +35,16 @@ public class Loops extends PApplet {
         switch (mode)
         {
             case 0:
-            {
-                float w = 200;
-                float h = 50;
-                rectMode(CENTER);                    
-                if (mouseX > cx -  (w /2) && mouseX < cx + (w/2) && mouseY > cy - (h/2) && mouseY < cy + (h/2))
+                fill(50, 255, 255);                    
+                if (mouseX < cx)
                 {
-                    fill(50, 255, 255);                
+                    rect(0, 0, cx, height);
                 }
                 else
                 {
-                    fill(200, 255, 255);
+                    rect(cx, 0, cx, height);
                 }
-                rect(cx, cy, w, h);
                 break;
-            }                
             case 1:
                 fill(50, 255, 255);                                    
                 if (mouseX < cx && mouseY < cy)
@@ -70,7 +66,7 @@ public class Loops extends PApplet {
                 break;
             case 2:
             {
-                int numRects = (int)(mouseX / 10.0f);
+                int numRects = (int)(mouseX / 30.0f);
                 float w = width / (float) numRects;
                 float cgap = 255 / (float) numRects;
                 for(int i = 0 ; i < numRects ; i ++)
@@ -82,6 +78,7 @@ public class Loops extends PApplet {
             }
             case 3:
             {
+                //int numCircles = (int)(mouseX / 10.0f);
                 int numCircles = (int)(mouseX / 10.0f);
                 float w = width / (float) numCircles;
                 float cgap = 255 / (float) numCircles;
@@ -90,52 +87,25 @@ public class Loops extends PApplet {
                     fill(cgap * i, 255, 255);
                     ellipse(w / 2 + (i * w), cy, w, w);
                 }
-                break;
-            }
-            case 4:
-            {
-                int numLines = 5;
-                float theta = TWO_PI / (float) numLines;
-                float radius = 100;
-                for(int i = 0 ; i < numLines ; i ++)
-                {
-                    float angle = theta * i;
-                    float x = sin(angle) * radius;
-                    float y = cos(angle) * radius;
-                    line(cx, cy, cx + x, cy + y);                    
-                }
             }
             break;
-            case 5:
+
+            case 4:
             {
-                rectMode(CORNER);
-                int numRects = (int) mouseX / 10;
-                float w = width / (float) numRects;
-                float cgap = 255 / (float) numRects;
-                for(int i = 0 ; i < numRects ; i ++)
+                /*float w = 200;
+                float h = 100;
+                rectMode(CENTER);
+
+                if (mouseX > cx - (w/2) && mouseX < cx + (w/2) && mouseY > cy - (h/2) && mouseY <cy + (h/2))
                 {
-                    fill(cgap * i, 255, 255);
-                    rect(i * w, i * w, w, w);
-                    rect(width - ((i + 1) * w), i * w, w, w);
+                    fill(127, 255, 255);
                 }
-                break;
-            }
-            case 6:
-            {
-                int numCircles = (int) mouseX / 10;
-                float cgap = 255 / (float) numCircles;
-                float gap = width / (float) numCircles;
-                float w = width;
-                for(int i = numCircles ; i >= 1 ; i --)
+                else
                 {
-                    fill(i * cgap, 255, 255);
-                    w = i * gap;
-                    ellipse(cx, cy, w, w);
-                    
-                }                
-            }
-            case 7:
-            {
+                    fill(188, 255, 255);
+                }
+                rect(cx, cy, w, h);*/
+
                 offset += (mouseX / 100);
                 int numCircles = 20;
                 float w = width / (float)numCircles;
@@ -149,22 +119,110 @@ public class Loops extends PApplet {
                         ellipse((w / 2) + w * j, (w / 2) + w * i, w, w);
                     }
                 }
+                
             }
+            break;
+
+            case 5:
+            {
+                fill(255, 255, 255);
+                stroke(255);
+                int numLines = 5;
+                float theta = TWO_PI / (float) numLines;
+                float radius = 100;
+                for(int i = 0; i < numLines; i++)
+                {
+                    float angle = theta * i;
+                    float x = sin(angle) * radius;
+                    float y = cos(angle) * radius;
+                    line(cx, cy, cx + x, cy + y); 
+                }
+            }
+            break;
+
+            case 6:
+            {
+                rectMode(CORNER);
+                int numRects = (int) (mouseX / 10.0f);
+                float w = width / (float) numRects;
+                float cgap = 255 / (float) numRects;
+
+                for (int i = 0 ; i < numRects ; i ++)
+                {
+                    fill (cgap * i, 255, 255);
+                    rect(i * w, i * w, w, w);
+                    //rect(width - ((i + 1) * w), i * w, w, w);
+
+                }
+
+            break;
+            }
+            case 7:
+            {
+                rectMode(CORNER);
+                int numRects = (int) (mouseX / 10.0f);
+                float w = width / (float) numRects;
+                float cgap = 255 / (float) numRects;
+
+                for (int i = 0 ; i < numRects ; i ++)
+                {
+                    fill (cgap * i, 255, 255);
+                    rect(i * w, i * w, w, w);
+                    rect(width - ((i + 1) * w), i * w, w, w);
+
+                }
+
+            break;
+            }
+
             case 8:
             {
+                int numCircles = (int) mouseX / 10;
+                float cgap = 255 / (float) numCircles;
+                float gap = width / (float) numCircles;
+                float w = width;
+
+                for (int i = numCircles ; i >= 1 ; i--)
+                {
+                    fill(i *cgap, 255, 255);
+                    w = i * gap;
+                    ellipse(cx, cy, w, w);
+                }
+            break;
+            }  
+            case 9:
+            {
+
                 int sides = (mouseX / 50);
                 float theta = TWO_PI / (float) sides;
                 float radius = 200;
                 stroke(255);
-                for(int i = 1 ; i <= sides ; i ++)
+                for (int i = 1 ; i <= sides ; i ++)
                 {
-                    float x1 = sin(theta * (i - 1)) * radius;
-                    float y1 = cos(theta * (i - 1)) * radius;
+                    float x1 = sin(theta * (i -1)) * radius;
+                    float y1 = cos(theta * (i -1)) * radius;
                     float x2 = sin(theta * i) * radius;
                     float y2 = cos(theta * i) * radius;
                     line(cx + x1, cy + y1, cx + x2, cy + y2);
                 }
-            }
+                /*offset += (mouseX / 100);
+                int numCircles = 20;
+                float w = width / (float)numCircles;
+                float cgap = 255 / (numCircles + numCircles);
+
+                for (int i = 0 ; i < numCircles ; i ++)
+                {
+                    for (int j = 0 ; j < numCircles ; j ++)
+                    {
+                        float c = ((cgap * (i + j )) + offset) % 255;
+                        fill(c, 255, 255);
+                        ellipse((w / 2) + w * j, (w / 2) + w * i, w, w);
+                    }
+                }*/
+            
+            } 
+            break;
         }
+        
     }
 }
